93beb6ae8493ac7ad0b41577112bc5733a35745f
Muhammad Zain <zain.ashiq613@gmail.com>
2025-08-14 20:46:51 +0500

    vercel file changes

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react';
 import Button from '../../components/shared/Button';
+import FrontEnd from '../../components/projects/FrontEnd';
+import BackEnd from '../../components/projects/BackEnd';
+import Laravel from '../../components/projects/Laravel';
+import AppScript from '../../components/projects/AppScript';
 
 function Projects() {
-  const [tab, setTab] = useState('Website designs');
+  const [tab, setTab] = useState('Front End');
   return (
     <section className="relative top-15 px-[150px] pb-12">
-      <section className="max-w-[1440px] mx-auto flex flex-col items-center gap-8 py-12">
+      <section className="max-w-[1440px] mx-auto flex flex-col gap-8 py-12">
         <h1 className="text-5xl font-semibold text-secondary w-full">
           Lets have a look at my <span className="text-primary">Projects</span>
         </h1>
         <div className="flex items-center gap-6">
-          {['Website designs', 'Landing Page’s', 'Dashboard Design', 'Mobile App'].map((item) => (
+          {['Front End', 'Back End', 'Laravel Php', 'App Script'].map((item) => (
             <div key={item}>
               <Button
                 cn={`border bg-transparent !text-black ${
@@ -25,10 +29,10 @@ function Projects() {
           ))}
         </div>
         <div className="w-full">
-          {/* <div className="w-full">{tab === 'Website designs' && <WebsiteDesigns />}</div> */}
-          {/* <div className="w-full">{tab === 'Landing Page’s' && <LandingPages />}</div> */}
-          {/* <div className="w-full">{tab === 'Dashboard Design' && <Dashboards />}</div> */}
-          {/* <div className="w-full">{tab === 'Mobile App' && <MobileApps />}</div> */}
+          <div className="w-full">{tab === 'Front End' && <FrontEnd />}</div>
+          <div className="w-full">{tab === 'Back End' && <BackEnd />}</div>
+          <div className="w-full">{tab === 'Laravel Php' && <Laravel />}</div>
+          <div className="w-full">{tab === 'App Script' && <AppScript />}</div>
         </div>
       </section>
     </section>
